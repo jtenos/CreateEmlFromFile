1bda6c422833ed0c573d582a22e6202debd53ee5
Joe Enos <joe@jtenos.com>
2024-08-28 10:46:14 -0700

    Update run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -3,5 +3,5 @@
 # Iterate over each PDF file in the current directory
 for pdf_file in *.pdf; do
   # Execute the node command with the current PDF file
-  node index "$pdf_file"
+  node index-pdf "$pdf_file"
 done
